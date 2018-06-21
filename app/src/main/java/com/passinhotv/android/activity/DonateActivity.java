08caf2c606252a404631743a94d1fe0c2cfbf0ce
unknown <rose91719@gmail.com>
2018-06-22 03:25:27 +0800

    working on list items

--- app/src/main/java/com/passinhotv/android/activity/DonateActivity.java
+++ app/src/main/java/com/passinhotv/android/activity/DonateActivity.java
@@ -1,28 +1,20 @@
 package com.passinhotv.android.activity;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 import com.passinhotv.android.R;
 
-public class ProfileDetailsActivity extends AppCompatActivity {
+public class DonateActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
-        setContentView(R.layout.activity_profile_details);
+        setContentView(R.layout.activity_donate);
     }
     public void goBack(View v){
         finish();
-    }
-    public void saveData(View v){
-        finish();
-    }
-    public void goDonate(View v){
-        Intent intent = new Intent(ProfileDetailsActivity.this, DonateActivity.class);
-        startActivity(intent);
     }
 }
